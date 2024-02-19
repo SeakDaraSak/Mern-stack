f072860cb0e9fb860a8928f59a88d5b8f61063c9
SeakDaraSak <seaksak2002@gmail.com>
2024-02-19 10:53:54 +0700

    List-Products

---
+++ frontend/src/data.ts
@@ -0,0 +1,27 @@
+import { Product } from "./types/Products";
+export const sampleProduct: Product[] = [
+  {
+    name: "Nike Slim Shirt",
+    slug: "nike-slim-shirt",
+    category: "Shirt",
+    image: "../public/images/b2.png",
+    brand: "Nike",
+    price: 120,
+    countInStock: 10,
+    description: "hight Quality Shirt",
+    rating: 4.5,
+    numReview: 10,
+  },
+  {
+    name: "Nike Slim Shirt",
+    slug: "nike-slim-shirt",
+    category: "Shirt",
+    image: "../public/images/b2.png",
+    brand: "Nike",
+    price: 120,
+    countInStock: 10,
+    description: "hight Quality Shirt",
+    rating: 4.5,
+    numReview: 10,
+  },
+];
